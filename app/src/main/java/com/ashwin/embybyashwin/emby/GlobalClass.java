70ad6cb8d4f0e665fd19626b91a4d776fc76ba93
Ashwin M <ashwinnambiarm@gmail.com>
2021-11-15 23:25:01 +0530

    Added Global Class

---
+++ app/src/main/java/com/ashwin/embybyashwin/emby/GlobalClass.java
@@ -0,0 +1,48 @@
+package com.ashwin.embybyashwin.emby;
+
+import android.widget.ImageView;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+
+import mediabrowser.apiinteraction.android.AndroidApiClient;
+
+public final class GlobalClass {
+    private static GlobalClass INSTANCE = null;
+    private AndroidApiClient m_apiClient;
+
+    private GlobalClass() {};
+
+    public static GlobalClass getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new GlobalClass();
+        }
+        return(INSTANCE);
+    }
+
+    public void setApiClient(final AndroidApiClient apiClient){
+        INSTANCE.m_apiClient = apiClient;
+    }
+
+    public AndroidApiClient getApiClient(){
+        return INSTANCE.m_apiClient;
+    }
+
+    public ImageLoader getImageLoader(){
+        return INSTANCE.m_apiClient.getImageLoader();
+    }
+
+    public void LoadImagetoView(String url, ImageView imageView){
+        INSTANCE.m_apiClient.getImageLoader().get(url, new ImageLoader.ImageListener() {
+            @Override
+            public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+                imageView.setImageBitmap(imageContainer.getBitmap());
+            }
+
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        });
+    }
+}
